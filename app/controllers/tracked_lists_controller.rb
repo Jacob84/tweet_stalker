4f9a4e5f03f14dd7c76401b174c2bbf732f3e2ea
Jacob84 <jacob.mendoza@gmail.com>
2015-03-09 20:57:25 +0100

    Some style changes after installing Rubocop

--- app/controllers/tracked_lists_controller.rb
+++ app/controllers/tracked_lists_controller.rb
@@ -3,15 +3,13 @@ class TrackedListsController < ApplicationController
     show_all = params[:show_all] == 'true'
     service = TrackedListsService.new
     lists = service.get_lists(1)
-    if !show_all
-      lists = lists.select { |l| l.tracked == true }
-    end
+    lists = lists.select { |l| l.tracked == true } unless show_all
     render json: lists
   end
 
   def post
     tracker = TrackedListsService.new
-    only_tracked = params['_json'].select { |l| l[:tracked] == "true" }
+    only_tracked = params['_json'].select { |l| l[:tracked] == 'true' }
     identifiers =  only_tracked.map { |l| l[:twitter_list_id] }
 
     tracker.add_lists(1, identifiers)
