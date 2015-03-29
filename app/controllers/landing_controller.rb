fdc09cba08c48b4aeca8ed1639c24485ec968beb
Jacob84 <jacob.mendoza@gmail.com>
2015-03-29 11:31:59 +0200

    Redirection from landing if logged_in

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,7 @@
 # LandingController
 class LandingController < ApplicationController
   def index
+    app_url = url_for(controller: 'single_page_application', action: 'index')
+    redirect_to app_url if logged_in?
   end
 end
